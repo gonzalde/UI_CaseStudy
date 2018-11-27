7691d16c1d9fdc8d9b0b302164e9a4f56785fabc
gonzalde <30518927+gonzalde@users.noreply.github.com>
2018-11-27 19:43:44 +0100

    Add group favorites heading

--- JS/AdvancedSearchHotel.js
+++ JS/AdvancedSearchHotel.js
@@ -32,3 +32,20 @@ function checkKeyword() {
     }
     return true;
 }
+
+function getFavorites(){
+    $(".group-favorites-group").hide();
+    $(".favorites-group").show();
+    $("#favorites-title").css("color","#646464");
+    $("#favorites-title").css("text-decoration","underline");
+    $("#group-favorites-title").css("color","white");
+    $("#group-favorites-title").css("text-decoration","none");
+}
+
+function getGroupFavorites(){
+    $(".favorites-group").hide();
+    $("#group-favorites-title").css("color","#646464");
+    $("#group-favorites-title").css("text-decoration","underline");
+    $("#favorites-title").css("color","white");
+    $("#favorites-title").css("text-decoration","none");
+}
